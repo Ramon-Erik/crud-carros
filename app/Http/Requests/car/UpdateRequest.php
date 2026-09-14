937ee735f22cca7460048fdfa827ff55597f1a96
Ramon Silva <ramonerik29a@gmail,com>
2026-09-14 14:10:09 -0300

    feat: validating before creating and updating

---
+++ app/Http/Requests/car/UpdateRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\car;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min_digits:3',
+            'model' => 'required',
+            'manufacture_year' => 'required|int',
+            'color' => 'required|string|max_digits:9'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome é obrigatório',
+            'name.min_digits' => 'O nome deve ter pelo menos 3 caracteres',
+            'model.required' => 'O modelo é obrigatório',
+            'manufacture_year.int' => 'O ano deve ser um valor inteiro',
+            'manufacture_year.required' => 'O ano é obrigatório',
+            'color.required' => 'O modelo é obrigatório',
+            'color.max_digits' => 'Cor de até 9 digitos (hexadecimal com # ou nome)',
+            'color.string' => 'A cor deve ser uma string'
+        ];
+    }
+}
